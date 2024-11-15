50cca6564f55f546470bc7ff38f58532c61445d8
Your Name <you@example.com>
2024-11-14 20:23:16 -0500

    Trabajo JS

---
+++ proyect0/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <h1>Bienvenidos</h1> ,
+    <input name="myInput" />,
+    <select>
+      <option value="item1">segun el esta facil._.</option>
+      <option value="item2">procedo a llorar</option>
+    </select>
+
+  );
+}
+
+export default App;
