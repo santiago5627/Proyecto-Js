d11c623688e2d2738917b75081b55a65d7dcf1c2
Your Name <you@example.com>
2024-11-18 11:00:13 -0500

    ._.

--- proyect0/src/App.js
+++ proyect0/src/App.js
@@ -1,14 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from '.';
 
 function App() {
   return (
-    <h1>Bienvenidos</h1> ,
-    <input name="myInput" />,
-    <select>
-      <option value="item1">segun el esta facil._.</option>
-      <option value="item2">procedo a llorar</option>
-    </select>
+  <Login  />
 
   );
 }
